52916f0f4ce2b4a75583b1b93343b760c4510d2b
Wander128 <wanderleybc128@gmail.com>
2019-12-09 19:49:22 -0500

    Regla
    Codigo con la regla

--- 4enlíneav.10/Assets/GameManager.cs
+++ 4enlíneav.10/Assets/GameManager.cs
@@ -11,11 +11,11 @@ public class GameManager : MonoBehaviour
     private int width = 10; // Alto
     private bool player1 = true; // Boleano que permite el cambio de jugador
     private bool winner; // Boleano que permite la declaración del ganador
-    public Text modoDeJuego;
     private bool horizontalWins;
     private bool verticalWins;
     private bool diagonalWins;
     private bool diagonalLeftWins;
+    [SerializeField] private Text ruleText;
 
 
     void Start()
@@ -25,6 +25,7 @@ public class GameManager : MonoBehaviour
 
     private void Setup()
     {
+        Regla();
         grid = new GameObject[width, heigth];
         for (var i = 0; i < heigth; i++)
         {
@@ -38,24 +39,23 @@ public class GameManager : MonoBehaviour
     }
 
     void Update()
-    { 
-        modoDeJuego.text= "hola"; //Con ésta línea del juego mostramos en pantalla que ha terminado el juego, pues hay un ganador.
+    {
+        //modoDeJuego.text= "hola"; //Con ésta línea del juego mostramos en pantalla que ha terminado el juego, pues hay un ganador.
 
         RestoreColor(); // La función llamada permite que al pasar el mouse las esferas retornen al color origen si no es seleccionada
 
         var mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition); // Permite la localización del mouse en el plano
 
-        if (!(mousePosition.x >= -0.5f) || !(mousePosition.x < width-0.5f) || !(mousePosition.y >= -0.5f) || 
-            !(mousePosition.y < heigth-0.5f)) return;
+        if (!(mousePosition.x >= -0.5f) || !(mousePosition.x < width - 0.5f) || !(mousePosition.y >= -0.5f) ||
+            !(mousePosition.y < heigth - 0.5f)) return;
 
         var x = (int)(mousePosition.x + 0.5f);
         var y = (int)(mousePosition.y + 0.5f);
-
         if (!winner)  // En caso de encontrar un ganador se detiene la ejecición del juego. 
-            {
+        {
             PickAPiece(x, y);
             if (Input.GetMouseButtonUp(0))
-            SelectColor(x,y);
+            SelectColor(x, y);
         }
     }
 
@@ -65,10 +65,10 @@ public class GameManager : MonoBehaviour
         {
             for (var j = 0; j < width; j++)
             {
-                    
+
                 if (grid[j, i].GetComponent<Renderer>().material.color == Color.black)
                 {
-                    grid[j,i].GetComponent<Renderer>().material.color = Color.white;
+                    grid[j, i].GetComponent<Renderer>().material.color = Color.white;
                 }
             }
         }
@@ -79,35 +79,34 @@ public class GameManager : MonoBehaviour
     ///  
     private void PickAPiece(int x, int y)
     {
-        if(grid[x,y].GetComponent<Renderer>().material.color == Color.white)
+        if (grid[x, y].GetComponent<Renderer>().material.color == Color.white)
         {
-            grid[x,y].GetComponent<Renderer>().material.color = Color.black;
+            grid[x, y].GetComponent<Renderer>().material.color = Color.black;
         }
     }
     /// <summary>
     /// En esta función se encuentran ubicadas los parámetros de juego, es decir, la selección el cambio de turno del jugador, indicar el jugador ganador, 
     /// </summary>
- 
+
     private void SelectColor(int x, int y)
     {
-        if (grid[x, y].GetComponent<Renderer>().material.color != Color.black) return; 
-        
-       var assignedColor = player1 ? Color.yellow : Color.green;
+        if (grid[x, y].GetComponent<Renderer>().material.color != Color.black) return;
 
-       grid[x,y].GetComponent<Renderer>().material.color = assignedColor;
+        var assignedColor = player1 ? Color.yellow : Color.green;
 
-        //int condicion= GetComponent<Renderer>() = UnityEngine.Random.Range(Regla);
+        grid[x, y].GetComponent<Renderer>().material.color = assignedColor;
 
         var thereIsAWinner = false;
+
         if (horizontalWins)
             thereIsAWinner = HorizontalConditions(x, y, assignedColor);
         if (!thereIsAWinner && verticalWins)
             thereIsAWinner = VerticalConditions(x, y, assignedColor);
-        if(!thereIsAWinner && diagonalWins)
+        if (!thereIsAWinner && diagonalWins)
             thereIsAWinner = DiagonalConditions(x, y, assignedColor);
         if (!thereIsAWinner && diagonalLeftWins)
             thereIsAWinner = DiagonalConditionsIz(x, y, assignedColor);
-        if (thereIsAWinner) 
+        if (thereIsAWinner)
         {
             var winnerName = player1 ? "Amarillo" : "Verde";
             DeclareWinner(winnerName);
@@ -118,23 +117,23 @@ public class GameManager : MonoBehaviour
     /// <summary>
     /// Con esta función declaramos el ganador.
     /// </summary>
-    
+
     private void DeclareWinner(string winnerName)
     {
         winner = true;
         Debug.Log(message: "El ganador es el " + winnerName);
-        //  jugador1.gameObject.SetActive(winner); //Con ésta línea del juego mostramos en pantalla que ha terminado el juego, pues hay un ganador.
+        //player1.gameObject.SetActive(winner); //Con ésta línea del juego mostramos en pantalla que ha terminado el juego, pues hay un ganador.
     }
 
     /// <summary>
     /// A partir de la línea 109 hasta la 208 se generan las condiciones para que Unity compare las esferas de forma horizontal, vertical, diagonal derecha e izquierda.
     /// En donde al no encontrar 4 esferas del mismo color el contador queda en 0 y continúa el juego.
     /// </summary>
-    
-    public bool HorizontalConditions (int x, int y, Color colorToCompare)
+
+    public bool HorizontalConditions(int x, int y, Color colorToCompare)
     {
         var counter = 0;
-        for (var i = x-3; i <= x+3; i++)
+        for (var i = x - 3; i <= x + 3; i++)
         {
             if (i < 0 || i >= width) continue;
 
@@ -150,12 +149,12 @@ public class GameManager : MonoBehaviour
             {
                 winner = true;
                 return winner;
-            }                                                             
+            }
         }
         return false;
     }
 
-    private bool VerticalConditions (int x, int y, Color colorToCompare)
+    private bool VerticalConditions(int x, int y, Color colorToCompare)
     {
         var counter = 0;
         for (var j = y - 3; j <= y + 3; j++)
@@ -170,7 +169,7 @@ public class GameManager : MonoBehaviour
             {
                 counter = 0;
             }
-            if (counter < 4) continue; 
+            if (counter < 4) continue;
             {
                 winner = true;
                 return winner;
@@ -205,7 +204,7 @@ public class GameManager : MonoBehaviour
         }
         return false;
     }
-    
+
     private bool DiagonalConditionsIz(int x, int y, Color colorToCompare)
     {
         var counter = 0;
@@ -253,7 +252,9 @@ public class GameManager : MonoBehaviour
                 diagonalLeftWins = true;
                 break;
         }
+        ruleText.text = text;
         return text;
     }
-    
 }
+    
+
