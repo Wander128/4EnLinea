83c3420aea5fd068f987e8f4cfde09a3ffbf9ef6
Wander128 <wanderleybc128@gmail.com>
2019-12-11 19:19:45 -0500

    VersionTerminada
    Juego terminado

--- 4enlíneav.10/Assets/GameManager.cs
+++ 4enlíneav.10/Assets/GameManager.cs
@@ -11,22 +11,24 @@ public class GameManager : MonoBehaviour
     private int width = 10; // Alto
     private bool player1 = true; // Boleano que permite el cambio de jugador
     private bool winner; // Boleano que permite la declaración del ganador
-    public Text jugador1;
-    private bool horizontalWins;
-    private bool verticalWins;
-    private bool diagonalWins;
-    private bool diagonalLeftWins;
-    [SerializeField] private Text ruleText;
+    public Text ganador; // Variable utilizada para ingresar el texto a mostrar en pantalla. 
+    private bool horizontalWins; // Variable tipo booleana que servirá para indicar un ganador de forma horizontal 
+    private bool verticalWins; // Variable tipo booleana que servirá para indicar un ganador de forma vertical
+    private bool diagonalWins; // Variable tipo booleana que servirá para indicar un ganador de forma diagonal
+    private bool diagonalLeftWins; // Variable tipo booleana que servirá para indicar un ganador de forma diagonal izquierda
+    [SerializeField] private Text ruleText; //Se crea una variable privada con SerializeField, que de igual forma aparecerá en el inspector para agregar el texto en pantalla
 
 
     void Start()
     {
         Setup(); // La función llamada contiene el código que crea la malla de esferas.
     }
-
+    /// <summary>
+    /// En este bloque de líneas (29-39) es donde se crea las esferas y asi mismo el tablero de juego de 10x10 
+    /// </summary>
     private void Setup()
     {
-        Regla();
+        Regla(); // La función llamada contiene los mensajes random del switch case creado 
         grid = new GameObject[width, heigth];
         for (var i = 0; i < heigth; i++)
         {
@@ -41,22 +43,23 @@ public class GameManager : MonoBehaviour
 
     void Update()
     {
-        //modoDeJuego.text= "hola"; //Con ésta línea del juego mostramos en pantalla que ha terminado el juego, pues hay un ganador.
-
         RestoreColor(); // La función llamada permite que al pasar el mouse las esferas retornen al color origen si no es seleccionada
-
-        var mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition); // Permite la localización del mouse en el plano
+        /// <summary>
+        /// Las líneas de codigo (51-62) Permiten la localización del mouse en el plano (tablero de juego) y de igual forma evita que al dar clic 
+        /// por fuera de este, afecte el juego
+        /// </summary>
+        var mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition); 
 
         if (!(mousePosition.x >= -0.5f) || !(mousePosition.x < width - 0.5f) || !(mousePosition.y >= -0.5f) ||
             !(mousePosition.y < heigth - 0.5f)) return;
 
         var x = (int)(mousePosition.x + 0.5f);
         var y = (int)(mousePosition.y + 0.5f);
-        if (!winner)  // En caso de encontrar un ganador se detiene la ejecición del juego. 
+        if (!winner) //En caso de encontrar un ganador se detiene la ejecición del juego. 
         {
-            PickAPiece(x, y);
+            PickAPiece(x, y); // La función llamada nos permitimos cambiar el color de la esfera sin seleccionarla, para así identificar la ubicación del mouse
             if (Input.GetMouseButtonUp(0))
-            SelectColor(x, y);
+            SelectColor(x, y); // La función llamada es la contiene el cambio de jugador y los demas parametros para jugar 
         }
     }
 
@@ -76,8 +79,7 @@ public class GameManager : MonoBehaviour
     }
     /// <summary>
     /// Con esta función nos permitimos cambiar el color de la esfera sin seleccionarla, para así identificar la ubicación del mouse
-    /// </summary>
-    ///  
+    /// </summary> 
     private void PickAPiece(int x, int y)
     {
         if (grid[x, y].GetComponent<Renderer>().material.color == Color.white)
@@ -86,12 +88,13 @@ public class GameManager : MonoBehaviour
         }
     }
     /// <summary>
-    /// En esta función se encuentran ubicadas los parámetros de juego, es decir, la selección el cambio de turno del jugador, indicar el jugador ganador, 
+    /// En esta función se encuentran ubicadas los parámetros de juego, es decir, la selección del cambio de turno del jugador, las condiciones que 
+    /// determina en cual de las direcciones se ganará y, además, el jugador ganador.
     /// </summary>
 
     private void SelectColor(int x, int y)
     {
-        if (grid[x, y].GetComponent<Renderer>().material.color != Color.black) return;
+        if (grid[x, y].GetComponent<Renderer>().material.color != Color.black) return; 
 
         var assignedColor = player1 ? Color.cyan : Color.magenta;
 
@@ -108,9 +111,10 @@ public class GameManager : MonoBehaviour
         if (!thereIsAWinner && diagonalLeftWins)
             thereIsAWinner = DiagonalConditionsIz(x, y, assignedColor);
         if (thereIsAWinner)
+
         {
-            var winnerName = player1 ? "Azul" : "Magenta";
-            DeclareWinner(winnerName);
+            var winnerName = player1 ? "Azul" : "Magenta"; 
+            DeclareWinner(winnerName); // Función llamada para mostrar el ganador 
         }
 
         player1 = !player1;
@@ -122,12 +126,12 @@ public class GameManager : MonoBehaviour
     private void DeclareWinner(string winnerName)
     {
         winner = true;
-        Debug.Log(message: "El ganador es el " + winnerName);
-        jugador1.gameObject.SetActive(winner); //Con ésta línea del juego mostramos en pantalla que ha terminado el juego, pues hay un ganador.
+        Debug.Log(message: "El ganador es el " + winnerName); // Se imprime el nombre del ganador
+        ganador.gameObject.SetActive(winner); //Con ésta línea del juego mostramos en pantalla que ha terminado el juego, pues hay un ganador.
     }
 
     /// <summary>
-    /// A partir de la línea 109 hasta la 208 se generan las condiciones para que Unity compare las esferas de forma horizontal, vertical, diagonal derecha e izquierda.
+    /// A partir de la línea 138 hasta la 238 se generan las condiciones para que Unity compare las esferas de forma horizontal, vertical, diagonal derecha e izquierda.
     /// En donde al no encontrar 4 esferas del mismo color el contador queda en 0 y continúa el juego.
     /// </summary>
 
@@ -233,10 +237,10 @@ public class GameManager : MonoBehaviour
         return false;
     }
     
-    public String Regla()
+    public String Regla() //Se crea una función tipo menú para la asignación de los mensajes que deberan aparecer en pantalla
     {
-        int randomName = UnityEngine.Random.Range(1, 4);
-        string text = "nulo";
+        int randomName = UnityEngine.Random.Range(1, 4); // Se crea una variable de nombre randomName y con el metodo switch se crea un menú que va desde el 1 al 4
+        string text = "nulo1";
         switch (randomName)
         {
             case 1:
@@ -253,8 +257,8 @@ public class GameManager : MonoBehaviour
                 diagonalLeftWins = true;
                 break;
         }
-        ruleText.text = text;
-        return text;
+        ruleText.text = text; // con rule text se mostrará en pantalla la dirección random a ganar 
+        return text; // Se retorna la variable text 
     }
 }
     
